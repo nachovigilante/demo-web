a21e9ccf6a8182665a2cbdfb1a8a3659015f2c1d
nachovigilante <nachovigilante@gmail.com>
2024-08-05 00:45:53 -0300

    feat: agrego varias paginas

--- script.js
+++ script.js
@@ -4,29 +4,27 @@ const secondsToMinutes = (seconds) => {
     return `${minutes}:${remainingSeconds.toString().padStart(2, "0")}`;
 };
 
-document.addEventListener("DOMContentLoaded", (event) => {
-    const listaCanciones = document.getElementById("lista-canciones");
+const cancionDOM = (cancion) => {
+    return `
+    <li class="cancion">
+        <img src="./assets/song.jpg" alt="Logo del álbum">
+        <div class="info">
+            <h3>${cancion.nombre}</h3>
+            <div class="bottom">
+                <span>${cancion.nombre_artista}</span>
+                <span>·</span>
+                <span>${secondsToMinutes(cancion.duracion)}</span>
+            </div>
+        </div>
+    </li>`;
+};
 
-    listaCanciones.innerHTML = "Cargando...";
-
-    fetch("http://localhost:8000/canciones")
-        .then((response) => response.json())
-        .then((data) => {
-            listaCanciones.innerHTML = "";
-            data.forEach((cancion) => {
-                const li = `
-                <li class="cancion">
-                    <img src="./assets/song.jpg" alt="Logo del álbum">
-                    <div class="info">
-                        <h3>${cancion.nombre}</h3>
-                        <div class="bottom">
-                            <span>Artista</span>
-                            <span>·</span>
-                            <span>${secondsToMinutes(cancion.duracion)}</span>
-                        </div>
-                    </div>
-                </li>`;
-                listaCanciones.innerHTML += li;
-            });
-        });
-});
+const artistaDOM = (artista) => {
+    return `
+    <li class="artista">
+        <img src="./assets/artist.jpg" alt="Logo del artista">
+        <div class="info">
+            <h3>${artista.nombre}</h3>
+        </div>
+    </li>`;
+};
